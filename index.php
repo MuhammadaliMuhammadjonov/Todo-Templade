cd471263a86e80c3929b99d49e93fdfcfad65a05
MuhammadaliMuhmmadjonov <muhammadaliwebdeveloper@gmail.com>
2024-01-12 21:21:53 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <!--begin::Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <!--end::Fonts-->
+    <!--begin::Page Vendors Styles(used by this page)-->
+    <link href="assets/plugins/custom/fullcalendar/fullcalendar.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
+    <!--end::Page Vendors Styles-->
+    <!--begin::Global Theme Styles(used by all pages)-->
+    <link href="assets/plugins/global/plugins.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/custom/prismjs/prismjs.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
+    <link href="assets/css/style.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
+    <!--end::Global Theme Styles-->
+    <!--begin::Layout Themes(used by all pages)-->
+    <link href="assets/css/themes/layout/header/base/light.css?v=7.0.3" rel="stylesheet" type="text/css" />
+    <link href="assets/css/themes/layout/header/menu/light.css?v=7.0.3" rel="stylesheet" type="text/css" />
+    <link href="assets/css/themes/layout/brand/dark.css?v=7.0.3" rel="stylesheet" type="text/css" />
+    <link href="assets/css/themes/layout/aside/dark.css?v=7.0.3" rel="stylesheet" type="text/css" />
+    <!--end::Layout Themes-->
+    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+
+
+    <div class="container col-xxl-12 d-flex flex-row-reverse p-2" >
+
+    <div class="col-xxl-6 order-2 order-xxl-1 ">
+        <!--begin::Advance Table Widget 2-->
+        <div class="card card-custom card-stretch gutter-b">
+            <!--begin::Header-->
+            <div class="card-header border-0 pt-5">
+                <h3 class="card-title align-items-start flex-column">
+                    <span class="card-label font-weight-bolder text-dark">New Arrivals</span>
+                    <span class="text-muted mt-3 font-weight-bold font-size-sm">More than 400+ new members</span>
+                </h3>
+                <!-- <div class="card-toolbar">
+                    <ul class="nav nav-pills nav-pills-sm nav-dark-75">
+                        <li class="nav-item">
+                            <a class="nav-link py-2 px-4" data-toggle="tab" href="#kt_tab_pane_1_1">Month</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link py-2 px-4" data-toggle="tab" href="#kt_tab_pane_1_2">Week</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link py-2 px-4 active" data-toggle="tab" href="#kt_tab_pane_1_3">Day</a>
+                        </li>
+                    </ul>
+                </div> -->
+            </div>
+            <!--end::Header-->
+            <!--begin::Body-->
+            <div class="card-body pt-3 pb-0">
+                <!--begin::Table-->
+                <div class="table-responsive">
+                    <table class="table table-borderless table-vertical-center">
+                        <thead>
+                            <tr>
+                                <th class="p-0" style="width: 50px"></th>
+                                <th class="p-0" style="min-width: 200px"></th>
+                                <th class="p-0" style="min-width: 125px"></th>
+                                <th class="p-0" style="min-width: 150px"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="pl-0 py-4">
+                                    <div class="symbol symbol-50 symbol-light mr-1">
+                                        <span class="symbol-label">
+                                            <img src="assets/media/svg/misc/006-plurk.svg"
+                                                class="h-50 align-self-center" alt="" />
+                                        </span>
+                                    </div>
+                                </td>
+                                <td class="pl-0">
+                                    <a href="#"
+                                        class="text-dark-75 font-weight-bolder text-hover-primary mb-1 font-size-lg">Sant
+                                        Outstanding</a>
+                                    <div>
+                                        <span class="font-weight-bolder">Email:</span>
+                                        <a class="text-muted font-weight-bold text-hover-primary"
+                                            href="#">bprow@bnc.cc</a>
+                                    </div>
+                                </td>
+                     
+                                <td class="text-right">
+                                    <span class="text-muted font-weight-500">ReactJs, HTML</span>
+                                </td>
+                 
+                                <td class="text-right pr-0">
+                                    <a href="#" class="btn btn-icon btn-light btn-hover-primary btn-sm">
+                                        <span class="svg-icon svg-icon-md svg-icon-primary">
+                                            <!--begin::Svg Icon | path:assets/media/svg/icons/General/Settings-1.svg-->
+                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px"
+                                                viewBox="0 0 24 24" version="1.1">
+                                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                    <rect x="0" y="0" width="24" height="24" />
+                                                    <path
+                                                        d="M7,3 L17,3 C19.209139,3 21,4.790861 21,7 C21,9.209139 19.209139,11 17,11 L7,11 C4.790861,11 3,9.209139 3,7 C3,4.790861 4.790861,3 7,3 Z M7,9 C8.1045695,9 9,8.1045695 9,7 C9,5.8954305 8.1045695,5 7,5 C5.8954305,5 5,5.8954305 5,7 C5,8.1045695 5.8954305,9 7,9 Z"
+                                                        fill="#000000" />
+                                                    <path
+                                                        d="M7,13 L17,13 C19.209139,13 21,14.790861 21,17 C21,19.209139 19.209139,21 17,21 L7,21 C4.790861,21 3,19.209139 3,17 C3,14.790861 4.790861,13 7,13 Z M17,19 C18.1045695,19 19,18.1045695 19,17 C19,15.8954305 18.1045695,15 17,15 C15.8954305,15 15,15.8954305 15,17 C15,18.1045695 15.8954305,19 17,19 Z"
+                                                        fill="#000000" opacity="0.3" />
+                                                </g>
+                                            </svg>
+                                            <!--end::Svg Icon-->
+                                        </span>
+                                    </a>
+                                    <a href="#" class="btn btn-icon btn-light btn-hover-primary btn-sm mx-3">
+                                        <span class="svg-icon svg-icon-md svg-icon-primary">
+                                            <!--begin::Svg Icon | path:assets/media/svg/icons/Communication/Write.svg-->
+                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px"
+                                                viewBox="0 0 24 24" version="1.1">
+                                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                    <rect x="0" y="0" width="24" height="24" />
+                                                    <path
+                                                        d="M12.2674799,18.2323597 L12.0084872,5.45852451 C12.0004303,5.06114792 12.1504154,4.6768183 12.4255037,4.38993949 L15.0030167,1.70195304 L17.5910752,4.40093695 C17.8599071,4.6812911 18.0095067,5.05499603 18.0083938,5.44341307 L17.9718262,18.2062508 C17.9694575,19.0329966 17.2985816,19.701953 16.4718324,19.701953 L13.7671717,19.701953 C12.9505952,19.701953 12.2840328,19.0487684 12.2674799,18.2323597 Z"
+                                                        fill="#000000" fill-rule="nonzero"
+                                                        transform="translate(14.701953, 10.701953) rotate(-135.000000) translate(-14.701953, -10.701953)" />
+                                                    <path
+                                                        d="M12.9,2 C13.4522847,2 13.9,2.44771525 13.9,3 C13.9,3.55228475 13.4522847,4 12.9,4 L6,4 C4.8954305,4 4,4.8954305 4,6 L4,18 C4,19.1045695 4.8954305,20 6,20 L18,20 C19.1045695,20 20,19.1045695 20,18 L20,13 C20,12.4477153 20.4477153,12 21,12 C21.5522847,12 22,12.4477153 22,13 L22,18 C22,20.209139 20.209139,22 18,22 L6,22 C3.790861,22 2,20.209139 2,18 L2,6 C2,3.790861 3.790861,2 6,2 L12.9,2 Z"
+                                                        fill="#000000" fill-rule="nonzero" opacity="0.3" />
+                                                </g>
+                                            </svg>
+                                            <!--end::Svg Icon-->
+                                        </span>
+                                    </a>
+                                    <a href="#" class="btn btn-icon btn-light btn-hover-primary btn-sm">
+                                        <span class="svg-icon svg-icon-md svg-icon-primary">
+                                            <!--begin::Svg Icon | path:assets/media/svg/icons/General/Trash.svg-->
+                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px"
+                                                viewBox="0 0 24 24" version="1.1">
+                                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                    <rect x="0" y="0" width="24" height="24" />
+                                                    <path
+                                                        d="M6,8 L6,20.5 C6,21.3284271 6.67157288,22 7.5,22 L16.5,22 C17.3284271,22 18,21.3284271 18,20.5 L18,8 L6,8 Z"
+                                                        fill="#000000" fill-rule="nonzero" />
+                                                    <path
+                                                        d="M14,4.5 L14,4 C14,3.44771525 13.5522847,3 13,3 L11,3 C10.4477153,3 10,3.44771525 10,4 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z"
+                                                        fill="#000000" opacity="0.3" />
+                                                </g>
+                                            </svg>
+                                            <!--end::Svg Icon-->
+                                        </span>
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!--end::Table-->
+            </div>
+            <!--end::Body-->
+        </div>
+        <!--end::Advance Table Widget 2-->
+    </div>
+
+    <div class="d-flex flex-column-fluid col-xxl-6" >
+        <!--begin::Container-->
+        <div class="container">
+            <!--begin::Card-->
+            <div class="card card-custom">
+              
+                <!--begin::Card body-->
+                <div class="card-body px-0">
+                    <form class="form" id="kt_form">
+                        <div class="tab-content">
+                            <!--begin::Tab-->
+                            <div class="tab-pane show active px-7" id="kt_user_edit_tab_1" role="tabpanel">
+                                <!--begin::Row-->
+                                <div class="row">
+                                    <div class="col-xl-2"></div>
+                                    <div class="col-xl-7 my-2">
+                                        <!--begin::Row-->
+                                        <div class="row">
+                                            <label class="col-3"></label>
+                                            <div class="col-9">
+                                                <h6 class="text-dark font-weight-bold mb-10">Customer Info:</h6>
+                                            </div>
+                                        </div>
+                                        <!--end::Row-->
+                                        <!--begin::Group-->
+                                        <div class="form-group row">
+                                            <label class="col-form-label col-3 text-lg-right text-left">Avatar</label>
+                                            <div class="col-9">
+                                                <div class="image-input image-input-empty image-input-outline"
+                                                    id="kt_user_edit_avatar"
+                                                    style="background-image: url(assets/media/users/blank.png)">
+                                                    <div class="image-input-wrapper"></div>
+                                                    <label
+                                                        class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow"
+                                                        data-action="change" data-toggle="tooltip" title=""
+                                                        data-original-title="Change avatar">
+                                                        <i class="fa fa-pen icon-sm text-muted"></i>
+                                                        <input type="file" name="profile_avatar"
+                                                            accept=".png, .jpg, .jpeg" />
+                                                        <input type="hidden" name="profile_avatar_remove" />
+                                                    </label>
+                                                    <span
+                                                        class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow"
+                                                        data-action="cancel" data-toggle="tooltip"
+                                                        title="Cancel avatar">
+                                                        <i class="ki ki-bold-close icon-xs text-muted"></i>
+                                                    </span>
+                                                    <span
+                                                        class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow"
+                                                        data-action="remove" data-toggle="tooltip"
+                                                        title="Remove avatar">
+                                                        <i class="ki ki-bold-close icon-xs text-muted"></i>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--end::Group-->
+                                        <!--begin::Group-->
+                                        <div class="form-group row">
+                                            <label class="col-form-label col-3 text-lg-right text-left">First
+                                                Name</label>
+                                            <div class="col-9">
+                                                <input class="form-control form-control-lg form-control-solid"
+                                                    type="text" value="Anna" />
+                                            </div>
+                                        </div>
+                                        <!--end::Group-->
+
+                                        <div class="form-group row">
+                                            <label class="col-form-label col-3 text-lg-right text-left"></label>
+                                            <div class="col-9 w-100 " style = "text-align:end">
+                                            <button class="btn btn-success">Add</button>
+
+                                            </div>
+                                        </div>
+                                        
+                                      
+                                    </div>
+                                </div>
+                                <!--end::Row-->
+                            </div>
+                            <!--end::Tab-->
+                          
+                        </div>
+                    </form>
+                </div>
+                <!--begin::Card body-->
+            </div>
+            <!--end::Card-->
+        </div>
+        <!--end::Container-->
+    </div>
+
+    </div>
+
+    <!--end::Demo Panel-->
+    <script>var HOST_URL = "https://keenthemes.com/metronic/tools/preview";</script>
+    <!--begin::Global Config(global config for global JS scripts)-->
+    <script>var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1200 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#3699FF", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#8950FC", "warning": "#FFA800", "danger": "#F64E60", "light": "#F3F6F9", "dark": "#212121" }, "light": { "white": "#ffffff", "primary": "#E1F0FF", "secondary": "#ECF0F3", "success": "#C9F7F5", "info": "#EEE5FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#212121", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#ECF0F3", "gray-300": "#E5EAEE", "gray-400": "#D6D6E0", "gray-500": "#B5B5C3", "gray-600": "#80808F", "gray-700": "#464E5F", "gray-800": "#1B283F", "gray-900": "#212121" } }, "font-family": "Poppins" };</script>
+    <!--end::Global Config-->
+    <!--begin::Global Theme Bundle(used by all pages)-->
+    <script src="assets/plugins/global/plugins.bundle.js?v=7.0.3"></script>
+    <script src="assets/plugins/custom/prismjs/prismjs.bundle.js?v=7.0.3"></script>
+    <script src="assets/js/scripts.bundle.js?v=7.0.3"></script>
+    <!--end::Global Theme Bundle-->
+    <!--begin::Page Vendors(used by this page)-->
+    <script src="assets/plugins/custom/fullcalendar/fullcalendar.bundle.js?v=7.0.3"></script>
+    <!--end::Page Vendors-->
+    <!--begin::Page Scripts(used by this page)-->
+    <script src="assets/js/pages/widgets.js?v=7.0.3"></script>
+
+</body>
+
+</html>
